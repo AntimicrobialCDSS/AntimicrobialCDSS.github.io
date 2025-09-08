41fb6893bbcf2837428d441d0cb69b7c1727d7e8
erikstensgard <98419189+erikstensgard@users.noreply.github.com>
2025-09-08 09:37:43 -0500

    OmahaGIUpdate

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const APP_PREFIX = 'CDSS_';
-const VERSION = '1.225'; // Update the version when you make changes
+const VERSION = '1.226'; // Update the version when you make changes
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const URLS = [
