b614bd02b1215a0264431fcb775d7199ed4c1f3a
AntimicrobialCDSS <146016810+AntimicrobialCDSS@users.noreply.github.com>
2026-04-23 08:47:49 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const APP_PREFIX = 'CDSS_';
-const VERSION = '1.248'; // Update the version when you make changes
+const VERSION = '1.249'; // Update the version when you make changes
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const URLS = [
